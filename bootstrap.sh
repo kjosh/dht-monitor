777df3da755fe4d89407a94e26b9db079b734f31
Joshua <kw.joshua@gmail.com>
2022-02-13 21:02:57 +0100

    run write_dht_values and kill child processes on exit

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 export FLASK_APP=./webapp/index.py
 source $(pipenv --venv)/bin/activate
+python3 write_dht_values.py &
 flask run -h 0.0.0.0
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
