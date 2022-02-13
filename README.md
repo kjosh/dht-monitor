7f43c90546288f34b62e44e9694bbd29cf3c9b9c
Joshua <kw.joshua@gmail.com>
2022-02-13 16:53:58 +0100

    initial

---
+++ README.md
@@ -0,0 +1,6 @@
+# DHT Monitor
+DHT Monitor offers a web interface to check the current and past temperature and humidity values
+read from a DHT22 sensor on a Raspberry Pi.
+
+# System Dependencies
+* libgpiod2 (`sudo apt-get install libgpiod2`)
---
+++ write_dht_values.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+import time
+import sqlite3
+import board
+import adafruit_dht
+
+INTERVAL = 5.0
+
+# DHT22 sensor on GPIO PIN 2
+dht_device = adafruit_dht.DHT22(board.D2, use_pulseio=False)
+
+# initialize database table
+connection = sqlite3.connect(':memory:')
+cursor = connection.cursor()
+cursor.execute('''
+    CREATE TABLE dht_values
+    (datetime text, temperature_c real, humidity real)
+''')
+connection.commit()
+
+# periodically write values to database
+while True:
+    try:
+        temperature_c = dht_device.temperature
+        humidity = dht_device.humidity
+        cursor.execute("INSERT INTO dht_values (?, ?, ?)", (datetime.now(), temperature_c, humidity))
+    except RuntimeError as error:
+        print(error.args[0])
+        time.sleep(INTERVAL)
+        continue
+    except Exception as error:
+        dht_device.exit()
+        connection.close()
+        raise error
+    time.sleep(INTERVAL)
