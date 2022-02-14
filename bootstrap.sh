ef9f4b83b01fb52d4d349df2602b2438487caffa
Joshua <kw.joshua@gmail.com>
2022-02-14 11:15:34 +0100

    move python sources

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-export FLASK_APP=./webapp/index.py
+export FLASK_APP=./py/webapp/index.py
 if [ $# -ge 1 ]; then
     export DHT_CFG_FILE=~/.dht-monitor.yaml
 else
     export DHT_CFG_FILE=$1
 fi
 source $(pipenv --venv)/bin/activate
-python3 write_dht_values.py &
+python3 py/write_dht_values.py &
 flask run -h 0.0.0.0
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
