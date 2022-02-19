40a96be67b7da19ce51deaac79a2c6491c3a0cf7
Joshua <kw.joshua@gmail.com>
2022-02-19 14:12:27 +0100

    fix: string to int conversion

--- py/webapp/index.py
+++ py/webapp/index.py
@@ -26,7 +26,7 @@ def data():
     hours = arg_or_default(request, "h", 0)
     minutes = arg_or_default(request, "m", 15)
     seconds = arg_or_default(request, "s", 0)
-    fetch_after = datetime.now() - timedelta(hours=hours, minutes=minutes, seconds=seconds)
+    fetch_after = datetime.now() - timedelta(hours=int(hours), minutes=int(minutes), seconds=int(seconds))
     values = cursor.execute(f"SELECT * FROM dht_values WHERE datetime >= :fetch_after", {"fetch_after": fetch_after}).fetchall()
     return jsonify(values)
 
@@ -38,7 +38,7 @@ def current_data_socket(ws):
             current = get_current()
             if not last_sent or last_sent[0] != current[0]:
                 last_sent = current
-                ws.send(json.dumps(current))            
+                ws.send(json.dumps(current))
         except sqlite3.OperationalError:
             pass # Database locked, retry
         time.sleep(5)
