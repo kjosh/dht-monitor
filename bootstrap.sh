38220e178f9707abd6ac4a2c9499f7290e3ab1c3
Joshua <kw.joshua@gmail.com>
2022-07-31 18:16:00 +0200

    run yarn build as sudo

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,7 +16,7 @@ source $(pipenv --venv)/bin/activate
 # remove old data
 python3 ${DIR}/py/cleanup_dht_values.py
 # build React app
-yarn --cwd ${DIR}/site run build
+sudo yarn --cwd ${DIR}/site run build
 # copy it to webserver dir
 sudo rm -rf /var/www/html/static
 sudo cp -r ${DIR}/site/build/* /var/www/html
