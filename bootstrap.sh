749e8bc66ea2e2891a719e2c393fdd1fad235bb5
Joshua <kw.joshua@gmail.com>
2022-07-31 17:30:17 +0200

    Run dht-monitor as a service
    - Build and copy react app to webserver-root instead of using react dev server
    - modifications to allow script to be ran from any directory

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-export FLASK_APP=./py/webapp/index.py
 if [ $# -lt 1 ]; then
 	echo "Using default config location"
 	export DHT_CFG_FILE=~/.dht-monitor.yaml
@@ -7,9 +6,23 @@ else
 	echo "Using specified config location $1"
 	export DHT_CFG_FILE=$1
 fi
+DIR="$(dirname "${BASH_SOURCE[0]}")"
+DIR="$(realpath "${DIR}")"
+
+echo "Running in ${DIR}"
+
+export PIPENV_PIPFILE=${DIR}/Pipfile
 source $(pipenv --venv)/bin/activate
-python3 py/cleanup_dht_values.py
-python3 py/write_dht_values.py &
-yarn --cwd ./site start &
-flask run -h 0.0.0.0
+# remove old data
+python3 ${DIR}/py/cleanup_dht_values.py
+# build React app
+yarn --cwd ${DIR}/site run build
+# copy it to webserver dir
+sudo rm -rf /var/www/html/static
+sudo cp -r ${DIR}/site/build/* /var/www/html
+
+# run flask server
+export FLASK_APP=${DIR}/py/webapp/index.py
+python3 ${DIR}/py/write_dht_values.py & flask run -h 0.0.0.0
+
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
